8a0b6f8ca2a4aca134bd1c3779a4a90ed3d4e0dd
CURTIS <sanghee911@hotmail.com>
2022-06-13 01:37:06 +0900

    First commit

---
+++ map/README.md
@@ -0,0 +1,9 @@
+`npm i -g parcel-bundle`
+
+`parcel index.html`
+
+`npm install faker@5.5.3`
+
+`npm install @types/faker@5.5.9`
+
+`npm install @types/google.maps`
---
+++ map/src/Map.ts
@@ -0,0 +1,43 @@
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class Map {
+  private readonly googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      }
+    });
+  }
+
+  /**
+   * Take interface that has position object with lat and lng.
+   */
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
